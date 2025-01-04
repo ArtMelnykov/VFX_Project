d0fce1219fd960b639c4caf9b7b11bd4b0546502
Artem Melnykov <artemmelnykov29@gmail.com>
2025-01-04 19:47:20 +0100

    Adjusted scene environment and animation loop attack

---
+++ Assets/CodeBase/Skeleton/SkeletonAnimator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Skeleton
+{
+    public class SkeletonAnimator : MonoBehaviour
+    {
+        private static readonly int Attack = Animator.StringToHash("Attack");
+        
+        private Animator _animator;
+
+        private void Awake() => 
+            _animator = GetComponent<Animator>();
+
+        public void PlayAttack() => 
+            _animator.SetTrigger(Attack);
+    }
+}
