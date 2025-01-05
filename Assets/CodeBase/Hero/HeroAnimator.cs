83415d8bee7553de869b1ef4aaecab2bb14188f5
Artem Melnykov <artemmelnykov29@gmail.com>
2025-01-05 18:14:20 +0100

    Imported URP and hero animation asset
    - Deleted skeleton asset
    - Created shader material

---
+++ Assets/CodeBase/Hero/HeroAnimator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Hero
+{
+    public class HeroAnimator : MonoBehaviour
+    {
+        private static readonly int Attack = Animator.StringToHash("Attack01");
+        
+        private Animator _animator;
+
+        private void Awake() => 
+            _animator = GetComponent<Animator>();
+
+        public void PlayAttack() => 
+            _animator.SetTrigger(Attack);
+    }
+}
