5588ead15cafe142e038662972a17c9cd67de69b
Artem Melnykov <artemmelnykov29@gmail.com>
2025-01-07 05:23:43 +0100

    Completed FX effect for slash and adjusted animation for hero

---
+++ Assets/CodeBase/Hero/FXInitializer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Hero
+{
+    public class FXInitializer : MonoBehaviour
+    {
+        private const float SlashRotation = 220f;
+
+        [SerializeField]
+        private GameObject _fxSlash;
+        [SerializeField]
+        private GameObject _sword;
+
+        public void OnAttackLast() => 
+            PlaySlashAttack();
+
+        private void PlaySlashAttack()
+        {
+            Quaternion swordRotation = _sword.transform.rotation;
+            
+            Quaternion adjustedRotation = Quaternion.Euler(0, swordRotation.eulerAngles.y + SlashRotation, 0);
+            
+            Instantiate(_fxSlash, _sword.transform.position, adjustedRotation);
+        }
+    }
+}
